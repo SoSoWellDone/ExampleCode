0d977ed8c443eaeb3fd414daae928b36db1d7ef1
SoSoWellDone <165465439+SoSoWellDone@users.noreply.github.com>
2024-04-05 19:03:21 +0200

    Update main.py
    Some changes in the main.py file

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-from openpyxl import Workbook
-wb = Workbook()
+"""Snippet code"""
 
-# grab the active worksheet
-ws = wb.active
-
-# Data can be assigned directly to cells
-ws['A1'] = 42
-
-# Rows can also be appended
-ws.append([1, 2, 3])
-ws.append((4, 5, 6))
-
-# Python types will automatically be converted
-import datetime
-ws['A2'] = datetime.datetime.now()
-
-# Save the file
-wb.save("sample.xlsx")
+print("Hello World!")
